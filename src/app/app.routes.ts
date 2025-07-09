bb00f1c822da3b1f32d6d603e2d0b5556b1faaee
Alexander Nole <cnole_azabache@outlook.com>
2025-07-09 03:26:29 -0500

    feat: add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './pages/inicio/inicio.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomePage } from './pages/home/home.page';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePage },
   { path: 'login', component: LoginComponent },
   { path: 'registro', component: RegisterComponent },
-  { path: 'inicio', component: InicioComponent },];
+  { path: 'inicio', component: InicioComponent },
+];
